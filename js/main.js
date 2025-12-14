a4f184417bf58be1fbc00978dd8b215fffbf5e54
florian-croiset <florian.croiset@epita.fr>
2025-12-14 02:55:47 +0100

    Add main.js with UI and utility imports

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// main.js
+import './ui/cursor.js';
+import './ui/navbar.js';
+import './ui/scroll.js';
+import './ui/buttons.js';
+import './ui/music.js';
+import './ui/modal.js';
+import './utils/countdown.js';
+import './utils/share.js';
+import './utils/clipboard.js';
+
+document.addEventListener('contextmenu', e => e.preventDefault());
