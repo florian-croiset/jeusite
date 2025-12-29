3e1d017da9f2ff0d0587a6f29ff9bcf766c8b760
florian-croiset <florian.croiset@epita.fr>
2025-12-29 22:15:31 +0100

    Update design.js

--- js/design.js
+++ js/design.js
@@ -265,13 +265,6 @@ document.addEventListener("DOMContentLoaded", () => {
             zoomable: true
         },
         {
-            title: "Logo E Nv Rond",
-            desc: "_",
-            img: "assets/pngLogoE.png",
-            download: "assets/pngLogoE.png",
-            zoomable: true
-        },
-        {
             title: "Logo E An Rond",
             desc: "_",
             img: "assets/pngLogoEP.png",
@@ -307,10 +300,10 @@ document.addEventListener("DOMContentLoaded", () => {
             zoomable: true
         },
         {
-            title: "Logo E cercle blanc",
+            title: "Logo E cercle",
             desc: "_",
-            img: "assets/pnglogoEcercleblanc.png",
-            download: "assets/pnglogoEcercleblanc.png",
+            img: "assets/pnglogoEcercle.png",
+            download: "assets/pnglogoEcercle.png",
             zoomable: true
         }
         /*
