5195b464c8050f67346b28666d2653e521dbec72
Florian Croiset <florian.croiset@epita.fr>
2026-01-04 00:13:13 +0100

    Refonte complète de test.html ; modif d'admin ; création de links pour les soutenances

---
+++ js/liens.js
@@ -0,0 +1,12 @@
+// main.js
+import './ui/cursor.js';
+import './ui/navbar.js';
+import './ui/scroll.js';
+import './ui/buttons.js';
+import './ui/music.js';
+import './ui/modal.js';
+import './utils/share.js';
+import './utils/clipboard.js';
+import './utils/settings.js';
+
+document.addEventListener('contextmenu', e => e.preventDefault());
